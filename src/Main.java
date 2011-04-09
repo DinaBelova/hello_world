a7f880efbb1657ec3b72e7275325dc539fda9214
DinaBelova <hiroine1991@gmail.com>
2011-04-09 17:21:16 +0400

    new superb methods added

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dina
+ * Date: 09.04.11
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    public static void printHello() {
+
+    }
+
+    public static void printWorld() {
+
+    }
+}
